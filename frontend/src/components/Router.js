d6b24afa01a7e4959ff6ebe2873ee4a3c183d657
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-03-02 01:39:47 +0500

    Update Router.js

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -5,16 +5,20 @@ import { Routes, Route } from "react-router-dom";
 import AddProduct from "./AddProduct";
 import GetProducts from "./GetProducts";
 import GetProduct from "./GetProduct";
+import SignUp from "./SignUP";
+import UserCart from "./UserCart";
 
 const Router = () => {
   return (
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="/add/product" element={<AddProduct />} />
         <Route path="/get/products" element={<GetProducts />} />
         <Route path="/get/product/:id" element={<GetProduct />} />
+        <Route path="/get/cart" element={<UserCart />} />
       </Routes>
     </>
   );
