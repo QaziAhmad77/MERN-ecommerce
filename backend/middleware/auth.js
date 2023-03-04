a4cf3e5eef92c430287740973c424dee18888fb2
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-03-04 21:13:51 +0500

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+module.exports = {
+  auth: async (req, res, next) => {
+    try {
+      if (!req.headers.authorization) {
+        return res.send({ code: 403, message: "Unauthorized Userssssss" });
+      }
+      const decoderUser = await jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+      // Date.now() is in milisecond and decoderUser.exp is in second so we divide Date.now() by 1000 to convert into second
+      if (Date.now() / 1000 >= decoderUser.exp) {
+        return res.send({ code: 403, message: "TOKEN_EXPIRED" });
+      }
+      //   req.user = decoderUser;
+      //   req.permissions = decoderUser.roles[0].permissions;
+      req["user"] = decoderUser;
+      req["permissions"] = decoderUser.roles[0].permissions;
+      next();
+    } catch (err) {
+      console.log(err);
+      return res.status(403).send("Token Expired");
+    }
+  },
+};
