a123611b8435dc44326715acd1e6cb38c6c1f772
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-03-05 01:49:49 +0500

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ module.exports = {
   auth: async (req, res, next) => {
     try {
       if (!req.headers.authorization) {
-        return res.send({ code: 403, message: "Unauthorized Userssssss" });
+        return res.send({ code: 403, message: "Unauthorized Usersssssss" });
       }
       const decoderUser = await jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
       // Date.now() is in milisecond and decoderUser.exp is in second so we divide Date.now() by 1000 to convert into second
