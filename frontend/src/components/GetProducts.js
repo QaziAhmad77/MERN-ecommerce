f7e8c631c776bb3c68a40de356736c7c09d4a184
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-03-08 19:32:12 +0500

    Update GetProducts.js

--- frontend/src/components/GetProducts.js
+++ frontend/src/components/GetProducts.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 const GetProducts = () => {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
   const [deleteData, setDeleteData] = useState([]);
   const [refresh, setRefresh] = useState(false);
-  console.log(deleteData, "deleteData");
+  const rights = JSON.parse(localStorage.getItem('rights'))[0].permissions;
   useEffect(() => {
+    const headers = { Authorization: localStorage.getItem('token') };
     axios
-      .get("http://localhost:4001/api/products/get-products")
+      .get('http://localhost:4001/api/products/get-products', { headers })
       .then((res) => {
-        console.log("getProduct", res.data);
+        console.log('getProduct', res.data);
         setData(res.data);
       })
       .catch((err) => {
@@ -21,12 +22,28 @@ const GetProducts = () => {
   }, [refresh]);
   const handleDelete = () => {
     const data = deleteData;
-    console.log(data, "Data");
+    console.log(data, 'Data');
     axios
-      .post("http://localhost:4001/api/products//delete-products", data)
+      .post('http://localhost:4001/api/products/delete-products', data)
       .then((res) => {
-        console.log(res.data, "25");
+        console.log(res.data, '25');
         if (res.data.code === 200) {
+          setRefresh(!refresh);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleAddCart = (productId) => {
+    const _productId = productId;
+    const userId = localStorage.getItem('userId');
+    const data = { productId: _productId, userId };
+    axios
+      .post('http://localhost:4001/api/users/add-to-cart', data)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.cdoe === 200) {
           setRefresh(!refresh);
         }
       })
@@ -36,26 +53,58 @@ const GetProducts = () => {
   };
   return (
     <>
-      <h1>
-        <strong>Products :</strong>
+      <h1 style={{ textAlign: 'center' }}>
+        <strong>SHOPPING CART PRODUCTS</strong>
       </h1>
+      <Link to="/get/cart">Go go cart</Link>
       {deleteData.length > 0 && (
         <button
           onClick={handleDelete}
-          style={{ height: "50px", margin: "0 auto", display: "block", backgroundColor: "#e91e63", color: "white", fontSize: "20px", borderRadius: "10px", border: "none" }}>
+          style={{
+            height: '50px',
+            margin: '0 auto',
+            display: 'block',
+            backgroundColor: '#e91e63',
+            color: 'white',
+            fontSize: '20px',
+            borderRadius: '10px',
+            border: 'none',
+          }}
+        >
           DELETE SELECTED
         </button>
       )}
-      <div style={{ display: "flex", flexWrap: "wrap", width: "100%", justifyContent: "center" }}>
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          width: '100%',
+          justifyContent: 'center',
+        }}
+      >
         {data &&
           data.length > 0 &&
           data.map((item, index) => {
             return (
-              <div style={{ margin: "50px 30px", background: "#eee", width: "27%" }} key={item._id}>
+              <div
+                style={{
+                  margin: '50px 30px',
+                  background: '#eee',
+                  width: '27%',
+                }}
+                key={item._id}
+              >
                 <div>
-                  <img style={{ width: "100%", height: "300px" }} src={item.image} alt="" />
+                  <img
+                    style={{ width: '100%', height: '300px' }}
+                    src={item.image}
+                    alt=""
+                  />
                 </div>
-                <div className="content" style={{ textAlign: "center", paddingBottom: "10px" }}>
+                <div
+                  className="content"
+                  style={{ textAlign: 'center', paddingBottom: '10px' }}
+                >
                   <p>
                     <strong>{item.name}</strong> in {item.category}
                   </p>
@@ -67,27 +116,39 @@ const GetProducts = () => {
                     <strong>PRICE : </strong>
                     {item.price} Only /-
                   </p>
+                  {rights.indexOf('edit product') !== -1 && (
+                    <button
+                      onClick={() => {
+                        navigate(`/get/product/${item._id}`);
+                      }}
+                    >
+                      EDIT
+                    </button>
+                  )}
+                  {rights.indexOf('delete products') !== -1 && (
+                    <input
+                      type="checkbox"
+                      style={{ marginLeft: '10px' }}
+                      onChange={(e) => {
+                        if (e.target.checked === true) {
+                          setDeleteData([...deleteData, item._id]);
+                        } else {
+                          setDeleteData(
+                            deleteData.filter((ele) => {
+                              return ele !== item._id;
+                            })
+                          );
+                        }
+                      }}
+                    />
+                  )}
                   <button
                     onClick={() => {
-                      navigate(`/get/product/${item._id}`);
-                    }}>
-                    EDIT
-                  </button>
-                  <input
-                    type="checkbox"
-                    style={{ marginLeft: "10px" }}
-                    onChange={(e) => {
-                      if (e.target.checked === true) {
-                        setDeleteData([...deleteData, item._id]);
-                      } else {
-                        setDeleteData(
-                          deleteData.filter((ele) => {
-                            return ele !== item._id;
-                          })
-                        );
-                      }
+                      handleAddCart(item._id);
                     }}
-                  />
+                  >
+                    ADD TO CART
+                  </button>
                 </div>
               </div>
             );
