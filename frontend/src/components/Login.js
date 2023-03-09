c1cf347d4c61d1fd6013c37d9978a212c57eda8d
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-03-09 20:43:42 +0500

    frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,27 +1,30 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useNavigate,Link } from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Login = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const data = { userName, password };
+  const data = {name: userName, password };
   const handleLogin = async () => {
     try {
       console.log(userName, password);
-      const res = await axios.post("http://localhost:4000/users", data);
+      const res = await axios.post(
+        'http://localhost:4001/api/users/login',
+        data
+      );
       if (res.data.token) {
-        localStorage.setItem("token", res.data.token);
-        localStorage.setItem("user",JSON.stringify(res.data.user));
-        localStorage.setItem("userId",res.data.user._id);
-        localStorage.setItem("rights",JSON.stringify(res.data.user.roles));
+        localStorage.setItem('token', res.data.token);
+        localStorage.setItem('user', JSON.stringify(res.data.user));
+        localStorage.setItem('userId', res.data.user._id);
+        localStorage.setItem('rights', JSON.stringify(res.data.user.roles));
         // if(res.data.user.type === "USER"){
         //   navigate("/home");
         // }
         // if(res.data.user.type === "SELLER"){
-          navigate("/get/products");
-      //   }
+        navigate('/get/products');
+        //   }
       }
     } catch (error) {
       console.log(error.message);
@@ -31,7 +34,9 @@ const Login = () => {
     <>
       <div>
         <h1>Login page</h1>
-        <div><Link to="/signup">Sign Up page</Link></div>
+        <div>
+          <Link to="/signup">Sign Up page</Link>
+        </div>
         USERNAME -
         <input
           type="text"
@@ -40,7 +45,7 @@ const Login = () => {
             console.log(e.target.value);
             setUserName(e.target.value);
           }}
-        />{" "}
+        />{' '}
         <br /> <br />
         PASSWORD -
         <input
