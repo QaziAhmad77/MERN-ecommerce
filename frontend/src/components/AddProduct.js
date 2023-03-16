e67759ea62181a99840da1570a4c4146b6bc479c
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-03-16 15:05:08 +0500

    Update AddProduct.js

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addProduct } from '../redux/actions/product.actions';
 
 const AddProduct = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [image, setImage] = useState('');
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [seller, setSeller] = useState('');
   const [price, setPrice] = useState('');
-  console.log(image, 12);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const headers = { Authorization: localStorage.getItem('token') };
@@ -19,58 +22,51 @@ const AddProduct = () => {
     formData.append('seller', seller);
     formData.append('price', price);
     formData.append('image', image);
-    axios
-      .post('http://localhost:4001/api/products/add-product', formData, {
-        headers,
-      })
-      .then((res) => {
-        console.log('addProduct', res.data);
-        if (res.status === 201) {
-          navigate('/get/products');
-        }
-      })
-      .catch((e) => console.log(e));
+    dispatch(addProduct(formData, headers));
+    navigate('/get/products');
   };
   return (
-    <div className='sign-up-form'>
+    <div className="sign-up-form">
       <h1>Add Product here</h1>
       <form action="" onSubmit={handleSubmit}>
         <input
           className="input-box"
-          placeholder='Product Name'
+          placeholder="Product Name"
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <input
           className="input-box"
-          placeholder='Category Name'
+          placeholder="Category Name"
           type="text"
           value={category}
           onChange={(e) => setCategory(e.target.value)}
         />
         <input
           className="input-box"
-          placeholder='Seller Name'
+          placeholder="Seller Name"
           type="text"
           value={seller}
           onChange={(e) => setSeller(e.target.value)}
         />
         <input
           className="input-box"
-          placeholder='Price'
+          placeholder="Price"
           type="number"
           value={price}
           onChange={(e) => setPrice(e.target.value)}
         />
         <input
           className="input-box"
-          placeholder='Image'
+          placeholder="Image"
           type="file"
           onChange={(e) => setImage(e.target.files[0])}
         />
         <br />
-        <button className='signup-btn' type="submit">Add</button>
+        <button className="signup-btn" type="submit">
+          Add
+        </button>
       </form>
     </div>
   );
