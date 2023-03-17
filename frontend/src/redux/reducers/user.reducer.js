914ae53993fbfdee0f45eb8253b3abc9a2041f3d
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-03-18 00:11:40 +0500

    Create user.reducer.js

---
+++ frontend/src/redux/reducers/user.reducer.js
@@ -0,0 +1,22 @@
+import * as types from '../actions/actionType';
+const initialState = {
+  user: {},
+  loading: true,
+};
+
+export const userReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case types.ADD_USER:
+      return {
+        ...state,
+        loading: false,
+      };
+    case types.LOGIN_USER:
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
