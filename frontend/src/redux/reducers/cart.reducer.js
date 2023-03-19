7f6b7f11f5919bda040418f6695e32c650cf98b8
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-03-19 23:05:57 +0500

    Create cart.reducer.js

---
+++ frontend/src/redux/reducers/cart.reducer.js
@@ -0,0 +1,35 @@
+import * as types from '../actions/actionType';
+const initialState = {
+  carts: [],
+  cart: {},
+  count: 0,
+  loading: true,
+};
+
+export const cartReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case types.ADD_CART:
+      console.log(action.payload);
+      return {
+        ...state,
+        count: action.payload2,
+        cart: action.payload1,
+        carts: [...state.carts, action.payload1],
+        Loading: false,
+      };
+    case types.DELETE_CART:
+      return {
+        ...state,
+        count: action.payload,
+        Loading: false,
+      };
+    case types.GET_SINGLE_CART:
+      return {
+        ...state,
+        cart: action.payload,
+        Loading: false,
+      };
+    default:
+      return state;
+  }
+};
