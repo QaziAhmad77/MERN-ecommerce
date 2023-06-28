5601a94b2545029150664bb99a2c36ca4458b01f
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-06-28 19:02:13 +0500

    frontend

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -11,13 +11,13 @@ const AddProduct = () => {
   const [category, setCategory] = useState('');
   const [seller, setSeller] = useState('');
   const [price, setPrice] = useState('');
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const headers = { Authorization: localStorage.getItem('token') };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name || !category || !seller || !price || !image) {
-      setError("Please fill all input fields");
+      setError('Please fill all input fields');
     } else {
       const formData = new FormData();
       formData.append('name', name);
@@ -27,56 +27,58 @@ const AddProduct = () => {
       formData.append('image', image);
       dispatch(addProduct(formData, headers));
       navigate('/get/products');
-      setError("");
+      setError('');
     }
   };
   return (
-   <div className="main3">
-     <div className="sign-up-form">
-      <h1>Add Product here</h1>
-      {error && <h3 style={{ color: "red", textAlign: "center" }}>{error}</h3>}
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          className="input-box"
-          placeholder="Product Name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          className="input-box"
-          placeholder="Category Name"
-          type="text"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <input
-          className="input-box"
-          placeholder="Seller Name"
-          type="text"
-          value={seller}
-          onChange={(e) => setSeller(e.target.value)}
-        />
-        <input
-          className="input-box"
-          placeholder="Price"
-          type="number"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
-        <input
-          className="input-box"
-          placeholder="Image"
-          type="file"
-          onChange={(e) => setImage(e.target.files[0])}
-        />
-        <br />
-        <button className="signup-btn" type="submit">
-          Add
-        </button>
-      </form>
+    <div className="main3">
+      <div className="sign-up-form">
+        <h1>Add Product here</h1>
+        {error && (
+          <h3 style={{ color: 'red', textAlign: 'center' }}>{error}</h3>
+        )}
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            className="input-box"
+            placeholder="Product Name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            className="input-box"
+            placeholder="Category Name"
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <input
+            className="input-box"
+            placeholder="Seller Name"
+            type="text"
+            value={seller}
+            onChange={(e) => setSeller(e.target.value)}
+          />
+          <input
+            className="input-box"
+            placeholder="Price"
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <input
+            className="input-box"
+            placeholder="Image"
+            type="file"
+            onChange={(e) => setImage(e.target.files[0])}
+          />
+          <br />
+          <button className="signup-btn" type="submit">
+            Add
+          </button>
+        </form>
+      </div>
     </div>
-   </div>
   );
 };
 
--- frontend/src/components/CardsDetails.js
+++ frontend/src/components/CardsDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams,useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteCart } from '../redux/actions/cart.actions';
@@ -9,7 +9,6 @@ const CardsDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const [image, setImage] = useState('');
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
@@ -36,93 +35,93 @@ const CardsDetails = () => {
     const userId = localStorage.getItem('userId');
     const data = { productId: _productId, userId };
     dispatch(deleteCart(data));
-        navigate("/get/products")
+    navigate('/get/products');
   };
   return (
     <>
-     <div className="main3">
-     <div className="container mt-2">
-        <h2 className="text-center">Items Details Page</h2>
-        <section className="container mt-3">
-          <div className="iteamsdetails"  style={{backgroundColor:"white"}}>
-            <div className="items_img">
-              <img src={`http://localhost:4001/${image}`} alt="" />
-            </div>
-            <div className="details">
-              <Table>
-                <tr>
-                  <td>
-                    <p>
-                      <strong>Name: </strong>
-                      {name}
-                    </p>
-                    <p>
-                      <strong>Category: </strong>
-                      {category}
-                    </p>
-                    <p>
-                      <strong>Seller: </strong> {seller}
-                    </p>
-                    <p>
-                      <strong>Price: </strong>: ₹ {price}
-                    </p>
-                    <p>
-                      <strong>Total Price</strong>: ₹ {price}
-                    </p>
-                    <div
-                      className="mt-5 d-flex justify-content-between align-items-center"
-                      style={{
-                        width: 100,
-                        cursor: 'pointer',
-                        background: '#ddd',
-                        color: '#111',
-                      }}
-                    >
-                      <span style={{ fontSize: 24 }}>-</span>
-                      <span style={{ fontSize: 22 }}></span>
-                      <span style={{ fontSize: 24 }}>+</span>
-                    </div>
-                  </td>
-                  <td>
-                    <p>
-                      <strong>Rating :</strong>
-                      <span
+      <div className="main3">
+        <div className="container mt-2">
+          <h2 className="text-center">Items Details Page</h2>
+          <section className="container mt-3">
+            <div className="iteamsdetails" style={{ backgroundColor: 'white' }}>
+              <div className="items_img">
+                <img src={`http://localhost:4001/${image}`} alt="" />
+              </div>
+              <div className="details">
+                <Table>
+                  <tr>
+                    <td>
+                      <p>
+                        <strong>Name: </strong>
+                        {name}
+                      </p>
+                      <p>
+                        <strong>Category: </strong>
+                        {category}
+                      </p>
+                      <p>
+                        <strong>Seller: </strong> {seller}
+                      </p>
+                      <p>
+                        <strong>Price: </strong>: ₹ {price}
+                      </p>
+                      <p>
+                        <strong>Total Price</strong>: ₹ {price}
+                      </p>
+                      <div
+                        className="mt-5 d-flex justify-content-between align-items-center"
                         style={{
-                          backgroundColor: 'green',
-                          color: '#fff',
-                          padding: '2px 5px',
-                          borderRadius: '5px',
+                          width: 100,
+                          cursor: 'pointer',
+                          background: '#ddd',
+                          color: '#111',
                         }}
                       >
-                        3.5 ★
-                      </span>
-                    </p>
-                    <p>
-                      <strong>Order Review :</strong>
-                      <span> 2525 + ordeer placed from here recently</span>
-                    </p>
-                    <p>
-                      <strong>Remove :</strong>
-                      <span>
-                        <li
-                          className="fas fa-trash"
-                          onClick={() => handleDelete(id)}
+                        <span style={{ fontSize: 24 }}>-</span>
+                        <span style={{ fontSize: 22 }}></span>
+                        <span style={{ fontSize: 24 }}>+</span>
+                      </div>
+                    </td>
+                    <td>
+                      <p>
+                        <strong>Rating :</strong>
+                        <span
                           style={{
-                            color: 'red',
-                            fontSize: '20px',
-                            cursor: 'pointer',
+                            backgroundColor: 'green',
+                            color: '#fff',
+                            padding: '2px 5px',
+                            borderRadius: '5px',
                           }}
-                        ></li>{' '}
-                      </span>
-                    </p>
-                  </td>
-                </tr>
-              </Table>
+                        >
+                          3.5 ★
+                        </span>
+                      </p>
+                      <p>
+                        <strong>Order Review :</strong>
+                        <span> 2525 + ordeer placed from here recently</span>
+                      </p>
+                      <p>
+                        <strong>Remove :</strong>
+                        <span>
+                          <li
+                            className="fas fa-trash"
+                            onClick={() => handleDelete(id)}
+                            style={{
+                              color: 'red',
+                              fontSize: '20px',
+                              cursor: 'pointer',
+                            }}
+                          ></li>{' '}
+                        </span>
+                      </p>
+                    </td>
+                  </tr>
+                </Table>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
-     </div>
     </>
   );
 };
